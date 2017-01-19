937fdac8494461c7bc00175557c8a910b433a320
Vinh Le <vinhljob@gmail.com>
2017-01-19 11:58:03 -0600

    Completed C# HelloWorld exercise

---
+++ csharp/hello-world/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism.hello_world
+{
+    class HelloWorld
+    {
+        public static string Hello(string name)
+        {
+            if(name == null)
+            {
+                return "Hello, World!";
+            }
+            else
+            {
+                return "Hello, " + name + "!";
+            }
+        }
+    }
+}
