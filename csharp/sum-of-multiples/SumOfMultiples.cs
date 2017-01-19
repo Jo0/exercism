403f9912865f0d103139b29a71548ac47b11c812
Vinh Le <vinhljob@gmail.com>
2017-01-19 16:39:11 -0600

    Completed Sum Of Multiples exercise

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+public class SumOfMultiples
+{
+    public static int To(int[] of, int to)
+    {
+        List<int> multiples = new List<int>();
+
+        for (int i = 0; i < of.Length; i++)
+        {            
+            for (int j = 3; j < to; j++)
+            {
+                if (j % of[i] == 0 && !multiples.Contains(j))
+                {
+                    multiples.Add(j);
+                }
+            }
+        }
+
+        int sum = 0;
+        foreach (var i in multiples)
+        {
+            sum += i;
+        }
+
+        return sum;
+
+
+    }
+}
