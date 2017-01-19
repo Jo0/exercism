1c0534edc401ece08a8f18a45bfefec030ec2bdc
Vinh Le <vinhljob@gmail.com>
2017-01-19 15:30:42 -0600

    Completed Rna Transcription exercise.

---
+++ csharp/rna-transcription/Complement.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+public class Complement
+{
+    public static string OfDna(string dna)
+    {
+        StringBuilder complement = new StringBuilder();
+        for(int i = 0; i < dna.Length; i++)
+        {
+            switch(dna[i])
+            {
+                case 'G':
+                    complement.Append('C');
+                    break;
+                case 'C':
+                    complement.Append('G');
+                    break;
+                case 'T':
+                    complement.Append('A');
+                    break;
+                case 'A':
+                    complement.Append('U');
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return complement.ToString();
+    }
+}
