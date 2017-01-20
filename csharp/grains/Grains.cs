e8ddd033df9c2e8c0b8225ba303a2ed413457762
Vinh Le <vinhljob@gmail.com>
2017-01-19 18:44:13 -0600

    Completed Grains exercise

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Grains
+{
+    public static ulong Square(int square)
+    {
+        return (ulong)Math.Pow(2, square-1);
+    }
+
+    public static ulong Total()
+    {
+        ulong sum = 0;
+        for(int i = 0; i <= 64; i++)
+        {
+            sum += (ulong)Math.Pow(2, i);
+        }
+
+        return sum;
+    }
+}
