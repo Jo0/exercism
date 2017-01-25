248ba052e678e85cea3791740023d50206f13360
Vinh Le <vinhljob@gmail.com>
2017-01-25 10:27:42 -0600

    Completed Raindrops exercise

---
+++ csharp/raindrops/Raindrops.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+public class Raindrops
+{
+    public static string Convert(int number)
+    {
+        StringBuilder sound = new StringBuilder();
+
+        if (number % 3 == 0)
+            sound.Append("Pling");
+
+        if (number % 5 == 0)
+            sound.Append("Plang");
+
+        if (number % 7 == 0)
+            sound.Append("Plong");
+
+        if (sound.Length == 0)
+            sound.Append(number);
+
+        return sound.ToString();
+    }
+}
