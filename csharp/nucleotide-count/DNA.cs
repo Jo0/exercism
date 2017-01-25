ae1cfe5242f2ac1d44bf74b1018e0618d52a7b97
Vinh Le <vinhljob@gmail.com>
2017-01-25 13:55:12 -0600

    Completed Nucleotide Count exercise

---
+++ csharp/nucleotide-count/DNA.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+internal class DNA
+{
+    private string v;
+    public Dictionary<char, int> NucleotideCounts = new Dictionary<char, int> { { 'A', 0 }, { 'T', 0 }, { 'C', 0 }, { 'G', 0 } };
+    //System.ArgumentException InvalidNucleotideException;
+    public DNA(string v)
+    {
+        this.v = v;
+        
+        foreach (var key in NucleotideCounts.Keys.ToList())
+        {
+            NucleotideCounts[key] = Count(key);
+        }
+    }
+
+    public int Count(char c)
+    {
+       
+
+        int count = 0;
+
+        for(int i = 0; i < v.Length; i++)
+        {
+            if (v[i] == c)
+                count++;
+        }
+
+        return count;
+    }
+}
